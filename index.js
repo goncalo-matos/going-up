763fc36b4e3ed86a12f6c9cbbc50e56c822fc5a5
matosg <goncalomatos3@gmail.com>
2019-04-08 10:42:19 +0100

    Loop song and add ding

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+const concurrently = require('concurrently');
 const player = require('play-sound')((opts = {}));
 
-const audio = player.play('./elevator.mp3');
+let elevator = player.play('./elevator.mp3', onElevatorFinished);
+let hasFinished = false;
+
+function onElevatorFinished(err) {
+    if (err && !err.killed) throw err;
+
+    if (!hasFinished) {
+        elevator = player.play('./elevator.mp3', onElevatorFinished);
+    }
+}
+
+const [, , ...actualArguments] = process.argv;
+const commandToExecute = actualArguments.join(' ');
+
+concurrently([commandToExecute], {
+    raw: true
+})
+    .catch(() => {})
+    .then(() => {
+        const ding = player.play('./ding.mp3', { detached: true });
+
+        hasFinished = true;
+
+        ding.unref();
+        elevator.kill();
+    });
